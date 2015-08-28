89160354a25a268255d5cdbd5189f9898e94752a
Ohgyun Ahn <ohgyun@gmail.com>
2015-08-28 16:15:41 +0900

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -8,7 +8,7 @@ sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh
 # https://github.com/rupa/z
 # z, oh how i love you
 mkdir -p ~/code/z
-curl https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
+curl -L https://raw.github.com/rupa/z/master/z.sh > ~/code/z/z.sh
 chmod +x ~/code/z/z.sh
 # also consider moving over your current .z file if possible. it's painful to rebuild :)
 
